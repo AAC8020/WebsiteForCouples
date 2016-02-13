6a5e100054b2269231816ef9574954a05acf03ec
AAC8020 <bc3520@gmail.com>
2016-02-13 11:45:15 +0800

    添加文件
    添加项目中所需的所有文件

---
+++ static/script.js
@@ -0,0 +1,12 @@
+function showtext()
+{
+	document.getElementById("textinput").style.display="";
+	document.getElementById("fileupload").style.display="none";
+
+}
+function showmedia()
+{
+	document.getElementById("textinput").style.display="none";
+	document.getElementById("fileupload").style.display="";
+
+}
